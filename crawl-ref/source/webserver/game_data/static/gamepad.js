377d1a6df87c649e700d713bada6b1697dc91f59
grubsteak <joel.herzog@att.net>
2020-11-24 04:38:21 -0600

    add rudimentary game controller support

--- crawl-ref/source/webserver/game_data/static/gamepad.js
+++ crawl-ref/source/webserver/game_data/static/gamepad.js
@@ -7,18 +7,161 @@ define(["jquery"],
  * - grubsteak
  * 
  * Controls:
+ * Left Thumbstick move & A...Precise Movement (maps to numpad)
+ * Right Thumbstick move......Coarse Movment   (maps to numpad)
+ * Left Trigger...............Auto Explore
+ * Right Trigger..............Auto Attack
+ * A..........................Numpad Thumbstick / Precise Movement
+ * X..........................Pickup items
+ * Y..........................Wait one turn
+ * Left thumbstick Button.....Go upstairs
+ * Right thumbstick button....Go downstairs
+ * Start......................Press Enter
+ * Select.....................Go to
  */
 function ($) {
     "use strict";
+    // constants //
+
     const THUMBSTICK_KEYMAP = [
                 ['1','2','3'],
                 ['4','5','6'],
                 ['7','8','9']
                 ]
-        
     const BUTTON_REFRESH_RATE = 200;
 
-    var gamepad = {gamepadCursorX:0,gamepadCursorY:0}
+
+    // style prototypes
+    const STYLE_MOD_THEME = {
+        'background':'white',
+        'text':'monospace',
+        'color': 'black',
+        'border': '6px',
+        'borderStyle': 'double',
+        
+    }
+    const STYLE_CENTER_TEXT = {
+        'text-align': 'center'
+    }
+
+    // actual styles
+    const MenuButtonStyle = Object.assign(Object.assign({
+            'display':'block',
+            'position':'absolute',
+            'right':'0',
+            'top':'0',
+            'width':'100px',
+            'height':'40px',
+            'opacity':'0.25'
+    }, STYLE_MOD_THEME), STYLE_CENTER_TEXT)
+
+    const MenuStyle = Object.assign(Object.assign({
+        'display':'block',
+        'position':'absolute',
+        'left':'50px',
+        'top':'0',
+        'width':'calc(100% - 300px)',
+        'height':'calc(100% - 200px)',
+        'padding-left':'50px',
+        'padding-right':'50px',
+    }, STYLE_MOD_THEME))
+
+    // state //
+    var GAMEPAD_CURSOR = {x:0, y:0} // taken from gamepad polling and used by dungeonRendererHook
+    
+    // DOM elements //
+    var Menu=$('<div><h1 center>Gamepad Settings</h1></div>').css(MenuStyle)
+    var MenuButton = $('<div>Controller Help</div>').css(MenuButtonStyle)
+                     .on("click", (e)=>{
+                        Menu.toggle()
+                    })
+
+    // Keybinds / actions
+
+    var actionRegistry = new Map()
+    function createAction(name, data) {
+        var action = {
+            'name': name,
+            'key': data.key,
+            'type': data.type || 'keypress'
+        }
+        actionRegistry.set(name, action)
+        
+        return action
+    }
+    function fireAction(action) {
+        $.event.trigger({ type : action.type, which : action.key })
+    }
+
+    const XBOX_BUTTON_NAMES = {
+        0: 'A',
+        1: 'B',
+        2: 'X',
+        3: 'Y',
+        6: 'left trigger',
+        7: 'right trigger',
+        8: 'select',
+        9: 'start',
+        10: 'left thumbstick',
+        11: 'right thumbstick',
+        12: 'dpad-up',
+        13: 'dpad-down',
+        14: 'dpad-left',
+        15: 'dpad-right',
+    }
+    
+    function keyCode(char) {
+        return char.charCodeAt(0)
+    }
+
+    function getButton(name) {
+        for(let BUTTON in XBOX_BUTTON_NAMES) {
+            if(XBOX_BUTTON_NAMES[BUTTON] !== undefined
+                && XBOX_BUTTON_NAMES[BUTTON] == name) {
+                return BUTTON;
+            }
+        }
+        throw new Error("No button with name `"+name+"'")
+    }
+
+    const BINDS = {
+        [getButton('X')]: createAction('get items', {key:keyCode('g')}),
+        [getButton('Y')]: createAction('wait one turn', {key:keyCode('.')}),
+        [getButton('left trigger')]: createAction('auto explore', {key:keyCode('o')}),
+        [getButton('right trigger')]: createAction('auto attack', {key:9, type:'keydown'}),
+        [getButton('left thumbstick')]: createAction('go upstairs', {key:keyCode('<')}),
+        [getButton('right thumbstick')]: createAction('go downstairs', {key:keyCode('>')}),
+        [getButton('start')]: createAction('enter', {key:13}),
+        [getButton('select')]: createAction('go to', {key:keyCode('G')}),
+    }
+
+    const MOVE_BUTTON = getButton('A');
+
+    function moveThumbstick() {
+        fireAction({key:keyCode(THUMBSTICK_KEYMAP[-GAMEPAD_CURSOR.y+1][GAMEPAD_CURSOR.x+1]), type:"keypress", name:"thumbstick"})
+    }
+
+    $('body').append(MenuButton)
+    $('body').append(Menu)
+    
+    Menu.append([
+        "* ---------------------- C O N T R O L S -----------------------",
+        "*        Button               Action",
+        "* Left Thumbstick move & A...Precise Movement (maps to numpad)",
+        "* Right Thumbstick move......Coarse Movment   (maps to numpad)",
+        "* Left Trigger...............Auto Explore",
+        "* Right Trigger..............Auto Attack",
+        "* A..........................Numpad Thumbstick / Precise Movement",
+        "* X..........................Pickup items",
+        "* Y..........................Wait one turn",
+        "* Left thumbstick Button.....Go upstairs",
+        "* Right thumbstick button....Go downstairs",
+        "* Start......................Press Enter",
+        "* Select.....................Go to"
+    ].join('<br/>'))
+
+
+    Menu.hide()
 
     function gamepadHandler(event, connecting) {
       var controller = event.gamepad;
@@ -43,19 +186,17 @@ function ($) {
                    
                     var gamepadCursorX = Math.floor(gamepadX+0.5)
                     var gamepadCursorY = Math.floor(gamepadY+0.5)
-                    gamepad.gamepadCursorX = gamepadCursorX
-                    gamepad.gamepadCursorY = gamepadCursorY
-                    var pressedThisFrame = [];
-                    var keyToPress = undefined;
-                    var whichKey = undefined;
-                    var type = 'keypress';
 
+                    GAMEPAD_CURSOR.x = gamepadCursorX
+                    GAMEPAD_CURSOR.y = gamepadCursorY
+                    
+                    var pressedThisFrame = []; // button indexes that are rising edge this frame are true (also repeat strokes)
             
                     for(var i=0; i<controller.buttons.length; i++) {
                         pressedThisFrame[i]=controller.buttons[i].pressed && !wasPressed[i];
                         buttonDecay[i]=buttonDecay[i]!==undefined ? buttonDecay[i] : 0 // populate it
                     }
-                    for(let [key, decay] of buttonDecay.entries()) {
+                    for(let [key] of buttonDecay.entries()) {
                         
                         if(controller.buttons[key].pressed && !pressedThisFrame[key]) {
                             buttonDecay[key] = Math.max(0,buttonDecay[key]-5)
@@ -67,7 +208,7 @@ function ($) {
                             buttonDecay[key] = BUTTON_REFRESH_RATE
                         }
                     }
-
+                    
                     if(rightStick) {
                         var rightStickPressing = true
 
@@ -82,55 +223,20 @@ function ($) {
                         rightStickTimeout = Math.max(0,rightStickTimeout - 1)
                         if(rightStickPressing) {
                             // send the keymapping
-                            keyToPress = THUMBSTICK_KEYMAP[-gamepadCursorY+1][gamepadCursorX+1]
-
+                            moveThumbstick()
                             // make right stick move character faster/slower based on for far out you drag the stick
                             rightStickTimeout = Math.max((1-(Math.sqrt(gamepadX**2 + gamepadY**2)**2))*150,20)
                         }
                     }
                     
-                    if(pressedThisFrame[0]) { // a
-                        var numpad_key = THUMBSTICK_KEYMAP[-gamepadCursorY+1][gamepadCursorX+1]
-                        keyToPress = numpad_key
-                    } else if(pressedThisFrame[2]) { // x
-                        keyToPress = 'g'
-                    } else if(pressedThisFrame[3]) { // y
-                        keyToPress = '.'
-                    } else if(pressedThisFrame[6]) { // left trigger
-                        keyToPress = 'o'
-                    } else if(pressedThisFrame[7]) { // right trigger
-                        type = 'keydown'
-                        whichKey = 9 // tab
-                    } else if(pressedThisFrame[10]) { // left thumbstick
-                        keyToPress = "<"
-                    } else if(pressedThisFrame[11]) { // right thumbstick
-                        keyToPress = ">"
-                    } else if(pressedThisFrame[9])  { // start
-                        whichKey = 13 // enter
-                    } else if (pressedThisFrame[14]) {
-                        whichKey = 37 // left
-                        type = 'keydown'
-                    } else if (pressedThisFrame[12]) {
-                        whichKey = -5 // up
-                        type = 'keyup'
-                    } else if (pressedThisFrame[15]) {
-                        whichKey = 39 // right
-                        type = 'keydown'
-                    } else if (pressedThisFrame[13]) {
-                        whichKey = 40 // down
-                        type = 'keyup'
-                    } else if (pressedThisFrame[8]) { // select
-                        keyToPress = 'G'
-                    }
-    
-                    if(whichKey === undefined) {
-                        if(keyToPress !== undefined) {
-                            whichKey = keyToPress.charCodeAt(0)
+                    for(let button in pressedThisFrame) {
+                        console.log(button, wasPressed[button], BINDS, BINDS[button])
+                        if(pressedThisFrame[button] && BINDS[button] && buttonDecay[button]>0) {
+                            fireAction(BINDS[button])
                         }
                     }
-                    
-                    if(whichKey !== undefined) {
-                        $.event.trigger({ type : type, which : whichKey });
+                    if(pressedThisFrame[MOVE_BUTTON]) {
+                        moveThumbstick()
                     }
 
                     // populate wasPressed for next frame to poll against
@@ -149,7 +255,7 @@ function ($) {
     function dungeonRendererHook(get_viewcenter, place_cursor) {
         setInterval(function () {
             const viewcenter = get_viewcenter()
-            place_cursor(viewcenter.x+gamepad.gamepadCursorX, viewcenter.y+gamepad.gamepadCursorY);
+            place_cursor(viewcenter.x+GAMEPAD_CURSOR.x, viewcenter.y+GAMEPAD_CURSOR.y);
         }, 100); // bad bad bad bad bad
     }
 
